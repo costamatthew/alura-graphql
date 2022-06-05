28e0e11e0a732b330a044a66059dabdbac619925
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-06-04 23:41:08 -0300

    feat: create mutations adicionaUser, atualizaUser and deletaUser

--- 1-graphql-apollo-server/api/user/resolvers/userResolvers.js
+++ 1-graphql-apollo-server/api/user/resolvers/userResolvers.js
@@ -4,6 +4,14 @@ const userResolvers = {
     user: (root, { id }, { dataSources }) =>
       dataSources.usersAPI.getUserById(id),
   },
+  Mutation: {
+    adicionaUser: async (root, user, { dataSources }) =>
+      dataSources.usersAPI.adicionaUser(user),
+    atualizaUser: async (root, novosDados, { dataSources }) =>
+      dataSources.usersAPI.atualizaUser(novosDados),
+    deletaUser: async (root, { id }, { dataSources }) =>
+      dataSources.usersAPI.deletaUser(id),
+  },
 }
 
 module.exports = userResolvers
